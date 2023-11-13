64a6b4f667000046bc4be52175506e1581b6cb41
Jan Hryniuk <jasiekhryniuk@gmail.com>
2023-11-14 00:19:56 +0100

    Refactoring

--- football_neural_network.py
+++ football_neural_network.py
@@ -1,56 +1,65 @@
 import pandas
 from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
 from tensorflow import keras
 from flask import Flask
 
-data = pandas.read_csv('football_data.csv')
-
-X = data.drop(['Opponent', 'Result'], axis=1)
-y = data['Result']
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-
-model = keras.Sequential([
-    keras.layers.Dense(16, activation='relu', input_shape=(X.shape[1],)),
-    keras.layers.Dense(1)
-])
-
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(X, y, epochs=100, batch_size=1, validation_data=(X, y))
-
-predictions = model.predict(X)
-results = []
-
-for i in predictions:
-    if i > -1.5 and i < -0.5:
-        results.append(-1)
-    elif i > 0.5 and i < 1.5:
-        results.append(1)
-    else:
-        results.append(0)
-
-number = 0
-for i in range(206):
-    if results[i] == y[i]:
-        number += 1
-percentage = number / 206 * 100
-print("Neural Network Accuracy: ", percentage, "%")
-
-football_list = data["Opponent"]
 
 
+def get_predictions(data):
+    x = data.drop(['Opponent', 'Result'], axis=1)
+    y = data['Result']
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
 
+    model = keras.Sequential([
+        keras.layers.Dense(16, activation='relu', input_shape=(x.shape[1],)),
+        keras.layers.Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x, y, epochs=100, batch_size=1, validation_data=(x, y))
+
+    return model.predict(x)
+
+
+def get_results(predictions):
+    res = []
+
+    for i in predictions:
+        if i > -1.5 and i < -0.5:
+            res.append(-1)
+        elif i > 0.5 and i < 1.5:
+            res.append(1)
+        else:
+            res.append(0)
+
+    return res
+
+
+def get_accuracy(results, data):
+    number = 0
+    for i in range(len(results)):
+        if results[i] == data['Result'][i]:
+            number += 1
+    return number / (len(results)) * 100
+
+
+football_data = pandas.read_csv('football_data.csv')
+football_predictions = get_predictions(football_data)
+football_results = get_results(football_predictions)
+print("Neural Network Accuracy: ", get_accuracy(football_results, football_data), "%")
+football_list = football_data["Opponent"]
 app = Flask(__name__)
+
 
 @app.route('/discipline/<string:discipline>/opponent/<string:opponent>')
 def check(discipline, opponent):
-    if discipline == 'footbal':
-        for i in range(206):
+    if discipline == 'football':
+        for i in range(len(football_list)):
             if football_list[i] == opponent:
-                if results[i] == 1:
+                if football_results[i] == 1:
                     return {"discipline": discipline, "opponent": opponent, "result": "Poland Wins!"}
-                elif results[i] == -1:
+                elif football_results[i] == -1:
                     return {"discipline": discipline, "opponent": opponent, "result": "Poland Lose!"}
                 else:
                     return {"discipline": discipline, "opponent": opponent, "result": "Draw!"}
